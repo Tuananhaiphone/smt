c692cb79c9157c7f286dfcf5141c8e95cc572d01
Tuananhaiphone <tuananhaiphone@gmail.com>
2024-12-20 20:45:58 +0700

    a

--- sw.js
+++ sw.js
@@ -1,13 +1,12 @@
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('pwa-cache').then((cache) => {
+    caches.open('my-pwa-cache').then((cache) => {
       return cache.addAll([
-        './', // Cập nhật tất cả tài nguyên cần cache
-        './index.html',
-        './style.css',
-        './manifest.json',
-        './icon-192x192.png', // Thêm các icon và tài nguyên khác
-        './icon-512x512.png'
+        '/',
+        '/index.html',
+        '/style.css',
+        '/icon-192x192.png',
+        '/icon-512x512.png'
       ]);
     })
   );
@@ -15,10 +14,8 @@ self.addEventListener('install', (event) => {
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
     })
   );
 });
-
-
